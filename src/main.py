c22527a1c24cca39067584e950b48fd36cfd599d
Yuto Watanabe <yuto.w51942@gmail.com>
2020-04-05 17:44:09 +0900

    added feature

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ Copyright 2020 YutoWatanabe
 import datetime
 import os
 import time
-from typing import Dict, List
+from typing import Dict, List, Optional
 
 import click
 import numpy as np
@@ -39,17 +39,36 @@ def main(line_token: str):
 
 def connect(line_token: str, save_dir: str):
     '''
-    APIに接続し前回取得した情報と比較し日時が変更されている場合LINE notifyにpostする
+    複数のスプリントを実行させる。
+    - Today total
+    - Now total
+
+    Args:
+        line_token (str): LINEのアクセストークン
+        save_dir (str): 一時データを保存するディレクトリパス
+    '''
+    today_total(line_token, save_dir)
+    now_total(line_token, save_dir)
+
+
+def today_total(line_token: str, save_dir: str):
+    '''
+    - 前回取得したデータと最新のデータを比較し日付が変わっている場合に
+        - 前回取得したデータと最新の陽患者数の増加数の計算
+        - その他様々なデータを取得
+        - 増加数をグラフ描画し保存。メタデータをjsonで保存。
+        - 増加数のグラフ描画とデータをLINEにpost
 
     Args:
         line_token (str): LINE notifyのアクセストークン
+        save_dir (str): 一時データを保存するディレクトリパス
     '''
     body = get_requests('https://covid19-japan-web-api.now.sh/api/v1/total')
     day = body['date']
 
     save_file_path = os.path.join(save_dir, 'save.json')
     daily_infections = os.path.join(save_dir, 'daily.json')
-    graph_image_path = os.path.join(save_dir, 'igraph.png')
+    graph_image_path = os.path.join(save_dir, 'graph.png')
 
     if os.path.isfile(save_file_path):
         old_body = json_read(save_file_path)
@@ -64,6 +83,7 @@ def connect(line_token: str, save_dir: str):
             daily = json_read(daily_infections)
         else:
             daily = []
+
         day_obj = datetime.datetime.strptime(str(day), r'%Y%m%d')
         daily.append({
             'day': day,
@@ -73,7 +93,7 @@ def connect(line_token: str, save_dir: str):
         text = f'''
 {day_obj.month}月{day_obj.day}日 更新
 
-感染者: {body['positive']}人 (前日比: {difference:+})
+☣感染者: {body['positive']}人 (前日比: {difference:+})
   - 退院: {body['discharge']}人
   - 入院中: {body['hospitalize']}人
     * 軽中度・無症状: {body['mild']}人
@@ -95,25 +115,69 @@ def connect(line_token: str, save_dir: str):
         json_write(body, save_file_path)
 
 
-def make_graph(daily: List[Dict[str, int]], image_save_path: str) -> None:
+def now_total(line_token: str, save_dir: str):
+    '''
+    感染者数の速報を通知する
+
+    Args:
+        line_token (str): LINEのアクセストークン
+        save_dir (str): 一時データを保存するディレクトリパス
+    '''
+    body = get_requests('https://covid19-japan-web-api.now.sh/api/v1/prefectures')
+
+    save_file_path = os.path.join(save_dir, 'now_total.json')
+    save_statistics = os.path.join(save_dir, 'now_infected.json')
+
+    if os.path.isfile(save_file_path):
+        old_body = json_read(save_file_path)
+        is_ratio = True
+    else:
+        old_body = {'patient': 0}
+        is_ratio = False
+
+    total_patient: int = 0
+    for element in body:
+        total_patient += int(element['cases'])
+
+    if total_patient != old_body['patient']:
+        if is_ratio:
+            difference = total_patient - old_body['patient']
+        else:
+            difference = 0
+
+        if os.path.isfile(save_statistics):
+            now = json_read(save_statistics)
+        else:
+            now = []
+        now.append({'day': datetime.datetime.now().strftime(r'%Y%m%d-%H'), 'infected': total_patient})
+
+        text = f'\n⚠感染者数更新: {total_patient}人 ({difference:+})'
+        post_line(line_token, text, None)
+        save_body = {'patient': total_patient}
+
+        json_write(save_body, save_file_path)
+        json_write(now, save_statistics)
+
+
+def make_graph(statistics: List[Dict[str, int]], image_save_path: str) -> None:
     '''
     国内の日別感染者数をグラフにして保存する
 
     Args:
-        daily (List[Dict[str, int]]): 日別感染者数のデータ
+        statistics (List[Dict[str, int]]): 感染者のデータ
         image_save_path (str): 出力する画像を保存するパス
     '''
     x_value = []
     y_value = []
 
-    for element in daily:
+    for element in statistics:
         day = datetime.datetime.strptime(str(element['day']), r'%Y%m%d')
         x_value.append(f'{day.month}/{day.day}')
         y_value.append(element['infected'])
 
     x_loc = np.array(range(len(y_value)))
 
-    pyplot.title('Daily infections')
+    pyplot.title('Infected person graph')
     pyplot.xlabel('date')
     pyplot.ylabel('people')
     pyplot.bar(x_loc, y_value, color='#fa6843')
@@ -136,19 +200,22 @@ def get_requests(link: str) -> Dict[str, int]:
     return response.json()
 
 
-def post_line(line_token: str, text: str, image_save_path: str):
+def post_line(line_token: str, text: str, image_save_path: Optional[str]):
     '''
     LINE notifyにpostする
 
     Args:
         line_token (str): LINE notifyのアクセストークン
         text (str): postする内容
-        image_save_path (str): 画像のパス
+        image_save_path (Optional[str]): 画像のパス。Noneの場合はpostしない。
     '''
     line_access_url = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': 'Bearer ' + line_token}
     payload = {'message': text}
-    files = {'imageFile': open(image_save_path, 'rb')}
+    if image_save_path is None:
+        files = None
+    else:
+        files = {'imageFile': open(image_save_path, 'rb')}
     requests.post(line_access_url, headers=headers, params=payload, files=files)
 
 
